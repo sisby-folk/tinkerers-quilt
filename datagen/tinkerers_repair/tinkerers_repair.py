227fc5ee581d6b94f2201e6692a8b62e5c1f29b5
sisby-folk <nora.herrah.heartstrings@gmail.com>
2022-07-29 16:32:23 +1000

    Add tinkerer's smithing Upgrading!

--- datagen/tinkerers_repair/tinkerers_repair.py
+++ datagen/tinkerers_repair/tinkerers_repair.py
@@ -28,22 +28,37 @@ armor_durability = [[165, 240, 225, 195],  # Iron
 tiers = [("iron", 250), ("golden", 32), ("diamond", 1561), ("netherite", 2031)]
 repair = ["iron_ingot", "gold_ingot", "diamond", "netherite_ingot"]
 types = ["helmet", "chestplate", "leggings", "boots", "pickaxe", "sword", "shovel", "axe", "hoe"]
+type_costs = [5, 8, 7, 4, 3, 2, 1, 3, 2]
 
-for template_name in ["dework", "repair"]:
-    with open("./template/" + template_name + ".json", "r") as template_file:
+for template_name in ["dework", "repair", "upgrade"]:
+    with open("./template/smithing.json", "r") as template_file:
         template = json.load(template_file)
 
         for type_idx, current_type in enumerate(types):
             for tier_idx, current_tier in enumerate(tiers):
-                item_string = "minecraft:" + current_tier[0] + "_" + current_type
-                repair_item_string = "minecraft:" + repair[tier_idx]
-                durability = current_tier[1] if type_idx > 3 else armor_durability[tier_idx][type_idx]
                 current_dict = deepcopy(template)
-                current_dict["base"]["item"] = item_string
-                current_dict["result"]["item"] = item_string
-                if template_name == "repair":
-                    current_dict["ingredient"]["item"] = repair_item_string
-                    current_dict["result"]["data"]["Damage"] = "$base.Damage - " + str(math.ceil(durability / 4))
+                match template_name:
+                    case "dework":
+                        current_dict["base"]["item"] = "minecraft:" + current_tier[0] + "_" + current_type
+                        current_dict["ingredient"]["item"] = "minecraft:netherite_scrap"
+                        current_dict["result"]["item"] = "minecraft:" + current_tier[0] + "_" + current_type
+                        current_dict["result"]["data"]["RepairCost"] = "$((base.RepairCost + 1)/2)-1"
+                        del current_dict["result"]["data"]["Damage"]
+                    case "repair":
+                        durability = current_tier[1] if type_idx > 3 else armor_durability[tier_idx][type_idx]
+                        current_dict["base"]["item"] = "minecraft:" + current_tier[0] + "_" + current_type
+                        current_dict["ingredient"]["item"] = "minecraft:" + repair[tier_idx]
+                        current_dict["result"]["item"] = "minecraft:" + current_tier[0] + "_" + current_type
+                        current_dict["result"]["data"]["Damage"] = "$base.Damage - " + str(math.floor(durability / 4.0))
+                        del current_dict["result"]["data"]["RepairCost"]
+                    case "upgrade":
+                        if current_tier == tiers[0] or type_idx <= 3:  # No iron or armor
+                            continue
+                        current_dict["base"]["item"] = "minecraft:" + tiers[tier_idx - 2 if current_tier[0] == "diamond" else 1][0] + "_" + current_type
+                        current_dict["ingredient"]["item"] = "minecraft:" + repair[tier_idx]
+                        current_dict["result"]["item"] = "minecraft:" + current_tier[0] + "_" + current_type
+                        current_dict["result"]["data"]["Damage"] = str(math.floor(current_tier[1] * ((type_costs[type_idx] - 1) / 4.0)))
+                        del current_dict["result"]["data"]["RepairCost"]
                 with open(dir_recipes + template_name + "_" + current_tier[0] + "_" + current_type + ".json",
                           "w") as out_file:
                     json.dump(current_dict, out_file, indent=4, sort_keys=True)
